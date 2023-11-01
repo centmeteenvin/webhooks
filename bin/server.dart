f21bf4a1297c89a854f0098e1c6666ce93bee3ae
vincent verbergt <vincent.verbergt@gmail.com>
2023-11-01 14:11:12 +0100

    Reduced version size

--- bin/server.dart
+++ bin/server.dart
@@ -3,30 +3,17 @@ import 'dart:io';
 import 'package:logger/logger.dart';
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart';
+import 'package:shelf_hotreload/shelf_hotreload.dart' as hotreload;
 import 'package:shelf_router/shelf_router.dart';
 
 part 'server.g.dart';
 
-final Logger logger= Logger(level: Level.info,);
-
-class EchoService {
-  Router get router => _$EchoServiceRouter(this);
-
-  @Route.get('/')
-  Future<Response> helloWorld(Request request) async {
-    return Response.ok("HomePage");
-  }
-
-  @Route.get('/<foo>')
-  Future<Response> echo(Request request, String foo) async {
-    return Response.ok(foo);
-  }
-}
+final Logger logger= Logger(level: Level.info, filter: ProductionFilter());
 
 class WebHookService {
   Router get router => _$WebHookServiceRouter(this);
 
-  @Route.post('/staging/webhook')
+  @Route.post('/webhook')
   Future<Response> echo(Request request) async {
     final body = await request.readAsString();
     logger.i(body);
@@ -37,14 +24,18 @@ class WebHookService {
 
 
 void main(List<String> args) async {
-  // Use any available host or container IP (usually `0.0.0.0`).
   final ip = InternetAddress.anyIPv4;
   final port = int.parse(args[1]);
+  hotreload.withHotreload(() => createServer(ip, port));
+}
+
+Future<HttpServer> createServer(InternetAddress ip, int port) async {
   final service = WebHookService();
   // Configure a pipeline that logs requests.
   final handler = Pipeline().addMiddleware(logRequests()).addHandler(service.router);
 
   // For running in containers, we respect the PORT environment variable.
   final server = await serve(handler, ip, port);
-  print('Listener Created on $ip:$port.');
+  logger.i('Listener Created on ${server.address}:${server.port}.');
+  return server;
 }
--- bin/server.g.dart
+++ bin/server.g.dart
@@ -6,26 +6,11 @@ part of 'server.dart';
 // ShelfRouterGenerator
 // **************************************************************************
 
-Router _$EchoServiceRouter(EchoService service) {
-  final router = Router();
-  router.add(
-    'GET',
-    r'/',
-    service.helloWorld,
-  );
-  router.add(
-    'GET',
-    r'/<foo>',
-    service.echo,
-  );
-  return router;
-}
-
 Router _$WebHookServiceRouter(WebHookService service) {
   final router = Router();
   router.add(
     'POST',
-    r'/staging/webhook',
+    r'/webhook',
     service.echo,
   );
   return router;
